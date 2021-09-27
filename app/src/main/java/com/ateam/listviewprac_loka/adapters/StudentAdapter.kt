86f230b42d51dd4af8f203faccf92e46bea5c6f2
Loka017 <jessinthespace@gmail.com>
2021-09-27 22:48:51 +0900

    출생년도 -> 한국식 나이(2021)로 계산해서 출력

--- app/src/main/java/com/ateam/listviewprac_loka/adapters/StudentAdapter.kt
+++ app/src/main/java/com/ateam/listviewprac_loka/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.ateam.listviewprac_loka.R
 import com.ateam.listviewprac_loka.datas.Student
 
@@ -24,6 +25,18 @@ class StudentAdapter(
 
         val row = tempRow!!
 
+        val studentData = mList[position]
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val birthYearTxt = row.findViewById<TextView>(R.id.birthYearTxt)
+
+        nameTxt.text = studentData.name
+
+//        출생년도를 가지고 => (2021년 한국식)나이로 변환해서 보여주기.
+        val koreanAge = 2021 - studentData.birthYear + 1
+
+//        birthYearTxt.text = "(${studentData.birthYear}년생)"
+        birthYearTxt.text = "(${koreanAge}세)"
         return row
     }
 
