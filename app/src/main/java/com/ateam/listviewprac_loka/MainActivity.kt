eaf34c73ab991b9cb328a8829d4e2977a3bcb426
Loka017 <jessinthespace@gmail.com>
2021-09-27 22:32:47 +0900

    어뎁터 변수 추가, 리스트뷰의 어뎁터로서 연결

--- app/src/main/java/com/ateam/listviewprac_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/listviewprac_loka/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ateam.listviewprac_loka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ateam.listviewprac_loka.adapters.StudentAdapter
 import com.ateam.listviewprac_loka.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val mStudentList = ArrayList<Student>()
+
+    lateinit var mAdapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,10 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(Student("이민규", 1966))
         mStudentList.add(Student("장소영", 1983))
         mStudentList.add(Student("오만수", 1998))
+
+        mAdapter = StudentAdapter(this,R.layout.student_list_item, mStudentList)
+
+        studentListView.adapter = mAdapter
 
     }
 }
