bd012c4000ced9052fdd5bf0b07a6865b4a83f7e
Loka017 <jessinthespace@gmail.com>
2021-09-27 22:14:51 +0900

    Adapter 뼈대 작업 마무리

--- app/src/main/java/com/ateam/listviewprac_loka/adapters/StudentAdapter.kt
+++ app/src/main/java/com/ateam/listviewprac_loka/adapters/StudentAdapter.kt
@@ -1,11 +1,31 @@
 package com.ateam.listviewprac_loka.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.ateam.listviewprac_loka.R
 import com.ateam.listviewprac_loka.datas.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student> ) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+
 }
